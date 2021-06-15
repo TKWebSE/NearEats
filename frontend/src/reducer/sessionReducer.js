8969a8f9e00f46549a64b7b3eef2e7599dfadbc9
TKWebSE <writetechcode@yahoo.co.jp>
2021-06-15 18:32:04 +0900

    kusa

--- frontend/src/reducer/sessionReducer.js
+++ frontend/src/reducer/sessionReducer.js
@@ -2,21 +2,24 @@ import { CardActions } from "@material-ui/core";
 import React from "react";
 
 export const initializeState = () => {
-    
+    sessionUser:[],
+    loggedIn:false,
 }
 
 export const sessionActionTypes = () => {
-
+    LOGINUSER:"LOGIJNUSER",
+    LOGOUTUSER:"LOGOUTUSER",
+    ISLOGIN:"ISLOGIN",
 }
 
 export const sessionReducer = (state,action) => {
     switch (action.type) {
-        case orderDetailActionTypes.FETCHING:
+        case sessionActionTypes.C:
             return {
                 ...state,
                 fetchstate:REQUEST_STATE.LOADING,
             }
-        case  orderDetailActionTypes.FETCH_SUCCESS:
+        case  sessionActionTypes.LOGOUTUSER:
             return {
                 fetchstate:REQUEST_STATE.OK,
                 order: action.payload.order,
