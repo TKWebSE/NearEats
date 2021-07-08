862438c4f9fc2ba037b6402cb3e61b73b00f63a2
TKWebSE <writetechcode@yahoo.co.jp>
2021-07-08 10:43:04 +0900

    kusa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,18 @@ class ApplicationController < ActionController::API
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     helper_method :current_api_v1_user, :api_v1_user_signed_in?
+    before_action :http_header_log
 
     # def current_user
     #     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     # end
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    end
+
+  private
+
+    def http_header_log
+      logger.info("api_version:#{request.headers[:HTTP_API_VERSION]}")
     end
 end
