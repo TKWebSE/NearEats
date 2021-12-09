51469a7972c3a8b84e5020d553eeb600605ebe74
TKWebSE <writetechcode@yahoo.co.jp>
2021-12-10 00:53:42 +0900

    kussa

--- frontend/src/component/MaterialUISaveButton.js
+++ frontend/src/component/MaterialUISaveButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -6,6 +6,7 @@ import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
 import Icon from '@material-ui/core/Icon';
 import SaveIcon from '@material-ui/icons/Save';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -13,20 +14,36 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export function SaveButton({onClick})  {
-    const classes = useStyles();
-  
-    return (
-      <div>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          startIcon={<SaveIcon />}
-          onClick={onClick}
-        >
-        Save
-      </Button>
-      </div>
-    )
+export function SaveButton({ onClick }) {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(false);
+
+  function handleClick() {
+    setLoading(true);
+    onClick();
   }
+
+  return (
+    <div>
+      {/* <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        startIcon={<SaveIcon />}
+        onClick={onClick}
+      >
+        Save
+      </Button> */}
+      <LoadingButton
+        color="primary"
+        onClick={handleClick}
+        loading={loading}
+        loadingPosition="start"
+        startIcon={<SaveIcon />}
+        variant="contained"
+      >
+        Save
+      </LoadingButton>
+    </div>
+  )
+}
